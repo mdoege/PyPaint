bd2b7694e59a2b3b9b3b05f2b241ee8d1bdbb7cc
Martin C. Doege <mdoege@compuserve.com>
2021-12-15 14:47:40 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+![logo](https://github.com/mdoege/PyPaint/raw/master/logo.gif "PyPaint logo")
+
+## PyPaint
+
+A very simple Python program for basic drawing and doodling
+
+Needs PyGame and NumPy
+
+### Main non-features
+
+* Fixed canvas size (1200x800 px)
+* Fixed brush size; only one drawing tool
+* 10 colors
+* No undo (but you can overpaint in white)
+* No image loading (to prevent George Lucas syndrome)
+* No output filename or file format selection
+
+### Usage
+
+Paint with the mouse. The mouse wheel selects a color (see colored square
+in top left corner).
+
+Press Space to erase drawing.
+
+Press Return to save as PNG in current directory.
+(Filename is date plus time.)
+
+### Samples
+
+![sample1](https://github.com/mdoege/PyPaint/raw/master/samples/advent.png "Advent wreath")
+![sample2](https://github.com/mdoege/PyPaint/raw/master/samples/bob_ross.png "Bob Ross landscape")
+![sample3](https://github.com/mdoege/PyPaint/raw/master/samples/computer.png "Computer")
+![sample4](https://github.com/mdoege/PyPaint/raw/master/samples/enjoy.png "Enjoy!")
+![sample5](https://github.com/mdoege/PyPaint/raw/master/samples/rabbit.png "Nice rabbit")
+![sample6](https://github.com/mdoege/PyPaint/raw/master/samples/star_trek.png "The Enterprise")
+![sample7](https://github.com/mdoege/PyPaint/raw/master/samples/trees.png "Happy little trees")
+
---
+++ pypaint.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+# Paint program
+
+import pygame, numpy, time
+
+# canvas size
+RES = 1200, 800
+
+# color palette in RGB hex:
+cols = (
+0,
+0xffffff,
+0x555753,
+
+0xcc0000,
+0xf57900,
+0xedd400,
+0x73d216,
+0x3465a4,
+0x75507b,
+0xc17d11,
+
+)
+
+class Paint:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(RES)
+        pygame.display.set_caption('Paint')
+        self.clock = pygame.time.Clock()
+        self.arr = numpy.empty(RES)
+        self.arr[:,:] = 0xffffff
+        self.mdown = False
+        self.col = 0
+        self.getcolpic()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: self.running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                self.arr[:,:] = 0xffffff
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                savepic = pygame.Surface(RES)
+                pygame.surfarray.blit_array(savepic, self.arr)
+                pygame.image.save(savepic, time.strftime("%y%m%d_%H%M%S.png"))
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.mdown = True
+            if event.type == pygame.MOUSEBUTTONUP:
+                self.mdown = False
+            if event.type == pygame.MOUSEWHEEL:
+                self.col += event.y
+                self.col = self.col % len(cols)
+                self.getcolpic()
+
+    def run(self):
+        self.running = True
+        while self.running:
+            self.clock.tick(50)
+            self.events()
+            self.update()
+        pygame.quit()
+
+    def getcolpic(self):
+        self.colpic = pygame.Surface((50, 50))
+        self.colpic.fill(cols[self.col])
+
+    def update(self):
+        if self.mdown:
+            x, y = pygame.mouse.get_pos()
+            self.arr[x-10:x+10,y-10:y+10] = cols[self.col]
+
+        pygame.surfarray.blit_array(self.screen, self.arr)
+        self.screen.blit(self.colpic, (0, 0))
+        pygame.display.flip()
+
+c = Paint()
+c.run()
+
